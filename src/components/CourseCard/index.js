8fbb7decc68c089eef71a15c32696cb8d9f32b3d
Baltazar <balti.kpo@gmail.com>
2022-04-18 21:38:59 -0300

    Added course card component

---
+++ src/components/CourseCard/index.js
@@ -0,0 +1,24 @@
+import { BiLink } from "react-icons/bi";
+
+export function CourseCard({ course }) {
+  return (
+    <a href={course.certificate} target="_blank" rel="noreferrer">
+      <div className="flex w-full items-center justify-between border-2 h-16 bg-indigo-100 dark:bg-indigo-700 border-indigo-100 dark:border-indigo-700 rounded-full gap:4 sm:gap-8 pr-4 sm:pr-8 shadow-sm hover:scale-105 transition-transform">
+        <div className="flex items-center h-full gap-4">
+          <img src={course.icon} alt="course" className="h-full rounded-full" />
+          <div className="flex flex-col h-full justify-center">
+            <h2 className="font-semibold text-sm sm:text-lg">{course.title}</h2>
+            <p className="font-bold text-sm sm:text-base text-indigo dark:text-indigo-300">
+              {course.author}
+            </p>
+          </div>
+        </div>
+        {course.certificate && (
+          <div className="text-indigo-400 dark:text-indigo-300 duration-200 hover:scale-110 hover:rotate-6">
+            <BiLink size="28px" />
+          </div>
+        )}
+      </div>
+    </a>
+  );
+}
